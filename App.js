a321245cf7c1de4664f9843f549286e2108c2be5
ricardogk3 <rgk19@hotmail.com>
2021-08-02 09:30:34 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';;
+import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, StatusBar} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from './src/components/HomeScreen';
+import Detalhes from './src/components/Detalhes';
+import Produtos from './src/components/Produtos';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar/>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Produtos" component={Produtos} />
+        <Stack.Screen name="Detalhes" component={Detalhes} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+ 
